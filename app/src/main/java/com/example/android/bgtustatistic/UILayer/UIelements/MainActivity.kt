06c942e43e9b45bd7c0b8f6d53603e3afe8d6600
tewat <hollywoodstreets23@gmail.com>
2022-01-02 18:08:49 +0300

    Bottom navigation implemented

---
+++ app/src/main/java/com/example/android/bgtustatistic/UILayer/UIelements/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.android.bgtustatistic.UILayer.UIelements
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.bgtustatistic.R
+import com.example.android.bgtustatistic.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding_: ActivityMainBinding? = null
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding_ = ActivityMainBinding.inflate(layoutInflater)
+        binding = binding_!!
+        setContentView(binding.root)
+
+        savedInstanceState?:let {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, MovementFragment())
+                .commit()
+        }
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.movement -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, MovementFragment())
+                        .commit()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.performance -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, PerformanceFragment())
+                        .commit()
+                    return@setOnItemSelectedListener true
+                }
+                else -> return@setOnItemSelectedListener false
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/bgtustatistic/UILayer/UIelements/MovementFragment.kt
@@ -0,0 +1,27 @@
+package com.example.android.bgtustatistic.UILayer.UIelements
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.android.bgtustatistic.R
+
+class MovementFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        Log.e("onCreateView", "here")
+        return inflater.inflate(R.layout.fragment_movement, container, false)
+    }
+
+}
