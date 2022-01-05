18199f1407d71783ea0c9d0c8eceada927dd0385
tewat <hollywoodstreets23@gmail.com>
2022-01-05 20:02:09 +0300

    LoginFragment.kt added

---
+++ app/src/main/java/com/example/android/bgtustatistic/UILayer/UIelements/LoginFragment.kt
@@ -0,0 +1,39 @@
+package com.example.android.bgtustatistic.UILayer.UIelements
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.android.bgtustatistic.R
+import com.example.android.bgtustatistic.databinding.FragmentLoginBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class LoginFragment : Fragment() {
+    private var binding_ : FragmentLoginBinding? = null
+    private lateinit var binding : FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding_ = FragmentLoginBinding.inflate(inflater)
+        binding = binding_!!
+
+        binding.loginButton.setOnClickListener {
+            requireActivity().run {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, PerformanceFragment())
+                    .commit()
+                val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+                bottomNav.visibility = View.VISIBLE
+            }
+        }
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding_ = null
+    }
+}
--- app/src/main/java/com/example/android/bgtustatistic/UILayer/UIelements/MainActivity.kt
+++ app/src/main/java/com/example/android/bgtustatistic/UILayer/UIelements/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.android.bgtustatistic.UILayer.UIelements
 
+import android.content.Context
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import com.example.android.bgtustatistic.R
 import com.example.android.bgtustatistic.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     private var binding_: ActivityMainBinding? = null
@@ -16,8 +23,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         savedInstanceState?:let {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.fragment_container, PerformanceFragment())
+//                .commit()
+            binding.bottomNavigation.visibility = View.GONE
             supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, PerformanceFragment())
+                .replace(R.id.fragment_container, LoginFragment())
                 .commit()
         }
 
@@ -38,5 +49,24 @@ class MainActivity : AppCompatActivity() {
                 else -> return@setOnItemSelectedListener false
             }
         }
+    }
+
+    // make EditText loose focus on tap outside (applies to Activity and Fragments)
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (ev?.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus ?: let {
+                return@dispatchTouchEvent super.dispatchTouchEvent(ev)
+            }
+            if (v is TextInputEditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(ev.rawX.toInt(), ev.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.windowToken, 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
